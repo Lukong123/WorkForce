c6b426a5d7c5921d54e222adaf4ae108317c5538
anne <annemachinda@gmail.com>
2022-07-11 12:43:42 +0100

    connected login page to the homepage

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,11 @@ from app import app
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
