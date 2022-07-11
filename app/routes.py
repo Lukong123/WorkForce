46931a4b2c1f807c10b9306f5b6961018c4750f3
anne <annemachinda@gmail.com>
2022-07-11 12:20:41 +0100

    reorganized images and styles

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
+from flask import Flask, render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World"
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
