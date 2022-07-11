4afcafdcb4e1b7bf87395c5b862851dfd53d8f4f
anne <annemachinda@gmail.com>
2022-07-11 13:37:23 +0100

    front end modification and creation of page signup

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,14 @@ def user_jobs():
 def user_profile():
     return render_template('user_profile.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signup_emp')
+def signup_emp():
+    return render_template('signup_emp.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
